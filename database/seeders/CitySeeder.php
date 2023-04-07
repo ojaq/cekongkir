60759b2e19e785a025cabb37aeab40d54acf98ba
ojaq <abangojaq@gmail.com>
2023-04-07 10:21:09 +0700

    import seed data with json file and minor changes

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\City;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,13 @@ class CitySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $fileCity = file_get_contents(base_path('/database/city.json'));
+        $fileRegency = file_get_contents(base_path('/database/regency.json'));
+
+        $dataCity = json_decode($fileCity, true);
+        $dataRegency = json_decode($fileRegency, true);
+
+        City::insert($dataCity);
+        City::insert($dataRegency);
     }
 }
